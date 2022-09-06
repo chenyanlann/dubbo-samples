1cb7b54daf6223a9e36174fa336d8a72a99adb7f
chenyanlann <32511cyl@gmail>
2022-09-06 22:35:13 +0800

    init

---
+++ dubbo-samples-xds/dubbo-samples-xds-provider/build-provider.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#cd ../../ && mvn clean && sleep 3s && mvn package -Dmaven.test.skip=true
+
+#cd deploy/deploy-provider/
+
+#echo '-----copy jar----'
+#rm -f dubbo-demo-provider.jar
+#cat Dockerfile
+
+#cp ../../dubbo-demo-provider/target/dubbo-demo-provider.jar .
+echo '-------build image-----------'
+docker build -f Dockerfile -t dockeddocking/dubbo:provider.agent.v1.16  .
+docker push dockeddocking/dubbo:provider.agent.v1.16
+echo '-------push image success-----------'
+
+docker rmi -f $(docker images|grep none |awk '{print $3}')
+echo '-----deploy images success----'
